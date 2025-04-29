0986441e68481a033d42ace1b70b101f63ba3761
qizong007 <978451432@qq.com>
2025-04-29 19:41:17 +0800

    强制推

--- components/GoogleAdsense.js
+++ components/GoogleAdsense.js
@@ -39,7 +39,7 @@ export default function GoogleAdsense() {
 
 /**
  * 文章内嵌广告单元
- * 请在GoogleAdsense后台配置创建对应广告，并且获取相应代码
+ * 请在 GoogleAdsense 后台配置创建对应广告，并且获取相应代码
  * 修改下面广告单元中的 data-ad-slot data-ad-format data-ad-layout-key(如果有)
  * 添加 可以在本地调试
  */
@@ -50,43 +50,43 @@ const AdSlot = ({ type = 'show' }) => {
   // 文章内嵌广告
   if (type === 'in-article') {
     return <ins className="adsbygoogle"
-            style={{ display: 'block', textAlign: 'center' }}
-            data-ad-layout="in-article"
-            data-ad-format="fluid"
-            data-adtest={siteConfig('ADSENSE_GOOGLE_TEST') ? 'on' : 'off'}
-            data-ad-client={siteConfig('ADSENSE_GOOGLE_ID')}
-            data-ad-slot={siteConfig('ADSENSE_GOOGLE_SLOT_IN_ARTICLE')}></ins>
+      style={{ display: 'block', textAlign: 'center' }}
+      data-ad-layout="in-article"
+      data-ad-format="fluid"
+      data-adtest={siteConfig('ADSENSE_GOOGLE_TEST') ? 'on' : 'off'}
+      data-ad-client={siteConfig('ADSENSE_GOOGLE_ID')}
+      data-ad-slot={siteConfig('ADSENSE_GOOGLE_SLOT_IN_ARTICLE')}></ins>
   }
 
   // 信息流广告
   if (type === 'flow') {
     return <ins className="adsbygoogle"
-            data-ad-format="fluid"
-            data-ad-layout-key="-5j+cz+30-f7+bf"
-            style={{ display: 'block' }}
-            data-adtest={siteConfig('ADSENSE_GOOGLE_TEST') ? 'on' : 'off'}
-            data-ad-client={siteConfig('ADSENSE_GOOGLE_ID')}
-            data-ad-slot={siteConfig('ADSENSE_GOOGLE_SLOT_FLOW')}></ins>
+      data-ad-format="fluid"
+      data-ad-layout-key="-5j+cz+30-f7+bf"
+      style={{ display: 'block' }}
+      data-adtest={siteConfig('ADSENSE_GOOGLE_TEST') ? 'on' : 'off'}
+      data-ad-client={siteConfig('ADSENSE_GOOGLE_ID')}
+      data-ad-slot={siteConfig('ADSENSE_GOOGLE_SLOT_FLOW')}></ins>
   }
 
   // 原生广告
   if (type === 'native') {
     return <ins className="adsbygoogle"
-            style={{ display: 'block', textAlign: 'center' }}
-            data-ad-format="autorelaxed"
-            data-adtest={siteConfig('ADSENSE_GOOGLE_TEST') ? 'on' : 'off'}
-            data-ad-client={siteConfig('ADSENSE_GOOGLE_ID')}
-            data-ad-slot={siteConfig('ADSENSE_GOOGLE_SLOT_NATIVE')}></ins>
+      style={{ display: 'block', textAlign: 'center' }}
+      data-ad-format="autorelaxed"
+      data-adtest={siteConfig('ADSENSE_GOOGLE_TEST') ? 'on' : 'off'}
+      data-ad-client={siteConfig('ADSENSE_GOOGLE_ID')}
+      data-ad-slot={siteConfig('ADSENSE_GOOGLE_SLOT_NATIVE')}></ins>
   }
 
   //  展示广告
   return <ins className="adsbygoogle"
-        style={{ display: 'block' }}
-        data-ad-client={siteConfig('ADSENSE_GOOGLE_ID')}
-        data-adtest={siteConfig('ADSENSE_GOOGLE_TEST') ? 'on' : 'off'}
-        data-ad-slot={siteConfig('ADSENSE_GOOGLE_SLOT_AUTO')}
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
+    style={{ display: 'block' }}
+    data-ad-client={siteConfig('ADSENSE_GOOGLE_ID')}
+    data-adtest={siteConfig('ADSENSE_GOOGLE_TEST') ? 'on' : 'off'}
+    data-ad-slot={siteConfig('ADSENSE_GOOGLE_SLOT_AUTO')}
+    data-ad-format="auto"
+    data-full-width-responsive="true"></ins>
 }
 
 export { AdSlot }
